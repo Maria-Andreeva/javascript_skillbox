04fa84ecd2d103268120eea74eb9c4edd2458a82
Maria.Andreeva <maria.andreeva.marketing@gmail.com>
2026-02-05 19:06:08 +0100

    added files blok13

---
+++ work/math/num1/script.js
@@ -0,0 +1,22 @@
+const giftArr = [
+    {
+      title: "Скидка 20% на первую покупку в нашем магазине!",
+      icon: "/img/discount.svg"
+    },
+    {
+      title: "Скидка 10% на всё!",
+      icon: "/img/discount_2.svg"
+    },
+    {
+      title: "Подарок при первой покупке в нашем магазине!",
+      icon: "/img/gift.svg"
+    },
+    {
+      title: "Бесплатная доставка для вас!",
+      icon: "/img/delivery.svg"
+    },
+    {
+      title: "Сегодня день больших скидок!",
+      icon: "/img/discount_3.svg"
+    }
+   ]
---
+++ work/math/num3/script.js
@@ -0,0 +1,14 @@
+const promocodeArr = [
+  {
+    promocode: 'PROM10',
+    gift: "Скидка 10%"
+  },
+  {
+    promocode: 'PROM50',
+    gift: "Скидка 50%"
+  },
+  {
+    promocode: 'GIFT',
+    gift: "Подарок в корзине"
+  }
+ ]
